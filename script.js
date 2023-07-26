a09478021f9ff8003f6ae9c220e64c6cfcdf7d85
edoinjp <edoinjp@gmail.com>
2023-07-26 10:34:18 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// var name = "academy";//string
+// var age = 21;
+// var x = true; // boolean true = 1 ; false=0;
+// var y = null; // null ;
+// var z ; // undefinded;
+
+
+// console.log(typeof age);
+
+
+
+// operatori
+
+// var x = 10 ;
+// var y = 5 ;
+// var z = x + y ;
+
+// console.log(z);
+
+// var names = ["girogi","levan",'beqa','mariami','maia',];
+            //  0        1       2       3    
+
+            // console.log(names[names.length - 1]);
+
+
+// 
+
+//object 
+
+// var user = {
+//     firstName: 'giorgi',
+//     lastName:'gvari',
+//     userAge: 23,
+//     phone: {
+//         code:558,
+//         phoneNumber:484900
+
+//     },
+//     Comments:[
+//         {id:1, comment:'hello1'}, //0
+//         {id:2, comment:'hello2'} //1
+//     ]
+// }
+// console.log(user.Comments[0].comment);
+
+// console.log(user.phone.phoneNumber); varianti 1 
+// console.log(user['phone']['code']); varianti 2 
+
+// console.log(user.lastName); 1 varianti   
+// console.log(user['firstName']); 2vrianti
+
+// var user = {
+//      names:'beqa',
+//      age:12
+
+// };
+
+// user.age = 19;
+// user.status = 'active';
+
+// console.log(user);
+
+//for
+
+let numbers = [ 100, 20 , 24, 3]
+ 
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// };
+
+
+//while
+
+let item = 0 ;
+while(item < numbers.length){
+    console.log(numbers[item]);
+    item++;
+}
